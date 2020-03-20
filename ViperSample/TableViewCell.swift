1811719f2977b85d221c1d1390c2f66706ad633d
NarenBhamla <narenbhamla@gmail.com>
2020-03-20 14:41:16 +0530

    Created New TableVIew File

---
+++ ViperSample/TableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TableViewCell.swift
+//  ViperSample
+//
+//  Created by Narander Kumar on 20/03/20.
+//  Copyright © 2020 Narander Kumar. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
--- ViperSample/ViewController.swift
+++ ViperSample/ViewController.swift
@@ -87,5 +87,7 @@ extension ViewController : ColorValueDelegate{
         
     }
     
+    // jcjsdhcdsjcvhjsdcbdjh
+    
     
 }
